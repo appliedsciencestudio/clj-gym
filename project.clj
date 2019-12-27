b64c594f7c5881ea3b72516c36775036492fd949
Dave Liepmann <dave.liepmann@gmail.com>
2019-12-27 10:15:54 +0100

    Switch to deps.edn; nix leiningen

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject mthomure/clj-gym "0.1.1-SNAPSHOT"
-  :description "Clojure Bindings for OpenAI's gym-http-api server"
-  :url "http://github.com/mthomure/clj-gym"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "2.3.0"]
-                 [cheshire "5.6.3"]])
